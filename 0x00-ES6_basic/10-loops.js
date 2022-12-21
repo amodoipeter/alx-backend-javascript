e98a61587cfd0a2e09a8f9f18f0718ffc5be7f3c
Amodoi Peter <amodoipeter@gmail.com>
2022-12-21 04:42:46 +0300

    for all loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
-//Rewrite the function appendToEachArrayValue to use ES6’s for...of operator. And don’t forget that var is not ES6-friendly.
 export default function appendToEachArrayValue(array, appendString) {
-    for (var idx in array) {
-      var value = array[idx];
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  // eslint-disable-next-line
+	for (const value of array) {
+    // eslint-disable-next-line
+		array[array.indexOf(value)] = appendString + value;
   }
+
+  return array;
+}
