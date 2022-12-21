0ab160aa3efaa7571a8be1e07f4bc99ac05f5c6f
Amodoi Peter <amodoipeter@gmail.com>
2022-12-21 04:58:49 +0300

    create employees object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,14 +1,3 @@
-//Write a function named createEmployeesObject that will receive two arguments:
-
-//departmentName (String)
-//employees (Array of Strings)
 export default function createEmployeesObject(departmentName, employees) {
-
-}
-//The function should return an object with the following format:
-
-return {
-    $departmentName: [
-         $employees,
-    ],
+    return { [departmentName]: [...employees] };
 }
