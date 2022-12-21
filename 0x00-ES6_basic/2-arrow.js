b7339f465877a3fc8f95627100f9a80d9e6b77a0
Amodoi Peter <amodoipeter@gmail.com>
2022-12-21 03:23:26 +0300

    arrows in js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,8 +2,8 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     const self = this;
-    //use ES6’s arrow syntax of the functio
     this.addNeighborhood = (newNeighborhood) => {
+      // use ES6’s arrow syntax of the function
       self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
     };
