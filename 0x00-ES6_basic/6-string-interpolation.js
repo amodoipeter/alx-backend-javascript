cbf3a98a2e2aba83f4610d6fbf4a5e737bfaefc4
Amodoi Peter <amodoipeter@gmail.com>
2022-12-21 03:58:21 +0300

    string interpolation

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,4 +1,3 @@
-//Rewrite the return statement to use a template literal so you can the substitute the variables you’ve defined.
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
@@ -7,7 +6,5 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return 'As of ' + year + ', it was the seventh-highest income county in the United States'
-        / ', with a per capita personal income of ' + budget.income + '. As of 2015, San Francisco'
-        / ' proper had a GDP of ' + budget.gdp + ', and a GDP per capita of ' + budget.capita + '.';
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
