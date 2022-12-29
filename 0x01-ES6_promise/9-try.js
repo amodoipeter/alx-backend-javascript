59a998faa0c8b8f09d66c85d0e7f8af0f8b0704a
Amodoi Peter <amodoipeter@gmail.com>
2022-12-29 06:13:11 +0300

    guardrail

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,3 +1,14 @@
-export default function guardrail(mathFunction){
-    
-}
+export default function guardrail(mathFunction) {
+    const queue = [];
+  
+    try {
+      queue.push(mathFunction());
+    } catch (err) {
+      queue.push(String(err));
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+  
+    return queue;
+  }
+  
