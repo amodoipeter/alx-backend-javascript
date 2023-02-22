308eeccec1027ad91d88dec018f3fc5596ca9819
Amodoi Peter <amodoipeter@gmail.com>
2023-02-22 06:04:03 +0300

    display messagee

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Amodoi Peter <https://github.com/amodoipeter>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
